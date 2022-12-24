a8d8450f05002ab3bdb2484c79344911dd5fded8
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2022-12-24 07:14:09 -0600

    Create ContentView.swift

---
+++ ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  StockTrackerPro
+//
+//  Created by Jayce Sagvold on 12/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        
+        
+        VStack {
+            
+            
+            List {
+                
+                Text("Your Favorite Stocks")
+                    .bold()
+                    .font(.title)
+                    .bold()
+                
+                
+                Text("APPL - 131.86")
+                    .foregroundColor(.red)
+                    .bold()
+                
+                Text("TSLA - 123.15")
+                    .foregroundColor(.red)
+                    .bold()
+                
+                Text("GOOG  89.81")
+                    .foregroundColor(.green)
+                    .bold()
+                
+                Text("MSFT - 238.73")
+                    .foregroundColor(.green)
+                    .bold()
+                
+                
+            }.navigationTitle("Your Favorite Stocks")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
